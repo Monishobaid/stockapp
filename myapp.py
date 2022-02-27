1cc061dee7c15af974acade2521907b6c4cb4a87
monish obaid <80661422+Monishobaid@users.noreply.github.com>
2022-02-27 11:06:45 +0530

    stockapp

---
+++ myapp.py
@@ -0,0 +1,21 @@
+from tracemalloc import start
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write(""" #Simple Stock Price App
+Shown are the stock closing price and volume""")
+#define the ticker symbol
+tickerSymbol = "AAPL"
+
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+#get the historical prices for this ticker
+
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+
+#open high low close volume dividends stock splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
